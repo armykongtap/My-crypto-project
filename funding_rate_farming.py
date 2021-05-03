bc42fb66980b5adc1f91f1ad5ffd372a139b2c8b
armykongtap <army_kongtap@hotmail.com>
2021-05-04 02:19:57 +0700

    funding rate farming using websocket

--- funding_rate_farming.py
+++ funding_rate_farming.py
@@ -1,8 +1,29 @@
-import config as cfg
-from binance.client import Client
-from binance.enums import ORDER_TYPE_LIMIT, SIDE_BUY, SIDE_SELL, TIME_IN_FORCE_GTC
+from dataclasses import dataclass
+from threading import Timer
+from typing import Callable
 
-client = Client(cfg.api_key, cfg.api_secret)
+from binance.client import Client
+from binance.enums import (
+    ORDER_TYPE_LIMIT,
+    ORDER_TYPE_MARKET,
+    SIDE_BUY,
+    SIDE_SELL,
+    TIME_IN_FORCE_GTC,
+)
+from binance.websockets import BinanceSocketManager
+
+import config as cfg
+
+# client = Client(cfg.api_key, cfg.api_secret)
+client = Client()
+bm = BinanceSocketManager(client)
+
+
+@dataclass
+class OrderBook:
+    best_bid: float = None
+    best_ask: float = None
+
 
 """
 Spot
@@ -35,15 +56,20 @@ def sell_limit(symbol: str, price: float, volume: float) -> dict:
     return order
 
 
-def get_orderbook_ticker(symbol: str) -> dict:
-    ob = client.get_orderbook_ticker(symbol=symbol)
-    return {
-        "symbol": ob["symbol"],
-        "bidPrice": float(ob["bidPrice"]),
-        "bidQty": float(ob["bidQty"]),
-        "askPrice": float(ob["askPrice"]),
-        "askQty": float(ob["askQty"]),
-    }
+def buy_market(symbol: str, volume: float) -> dict:
+    print("Buy market", symbol, volume)
+    order = client.create_order(
+        symbol=symbol, side=SIDE_BUY, type=ORDER_TYPE_MARKET, quantity=volume,
+    )
+    return order
+
+
+def sell_market(symbol: str, volume: float) -> dict:
+    print("Sell market", symbol, volume)
+    order = client.create_order(
+        symbol=symbol, side=SIDE_SELL, type=ORDER_TYPE_MARKET, quantity=volume,
+    )
+    return order
 
 
 """
@@ -77,15 +103,59 @@ def futures_short_limit(symbol: str, price: float, volume: float) -> dict:
     return order
 
 
-def futures_orderbook_ticker(symbol: str) -> dict:
-    ob = client.futures_orderbook_ticker(symbol=symbol)
-    return {
-        "symbol": ob["symbol"],
-        "bidPrice": float(ob["bidPrice"]),
-        "bidQty": float(ob["bidQty"]),
-        "askPrice": float(ob["askPrice"]),
-        "askQty": float(ob["askQty"]),
-    }
+def futures_long_market(symbol: str, volume: float) -> dict:
+    print("Long market", symbol, volume)
+    order = client.futures_create_order(
+        symbol=symbol, side=SIDE_BUY, type=ORDER_TYPE_MARKET, quantity=volume,
+    )
+    return order
+
+
+def futures_short_market(symbol: str, volume: float) -> dict:
+    print("Short market", symbol, volume)
+    order = client.futures_create_order(
+        symbol=symbol, side=SIDE_SELL, type=ORDER_TYPE_MARKET, quantity=volume,
+    )
+    return order
+
+
+"""
+Stream
+"""
+
+
+def sub_symbol_ticker_socket(symbol: str, trigger_func: Callable) -> OrderBook:
+    ob = OrderBook()
+
+    def process_message(msg):
+        if ob.best_bid == float(msg["b"]) and ob.best_ask == float(msg["a"]):
+            return
+        ob.best_bid = float(msg["b"])
+        ob.best_ask = float(msg["a"])
+        trigger_func()
+
+    conn_key = bm.start_symbol_ticker_socket(symbol, process_message)
+    print(f"Subscribe Spot: {conn_key}")
+
+    return ob
+
+
+def sub_symbol_ticker_futures_socket(symbol: str, trigger_func: Callable) -> OrderBook:
+    ob = OrderBook()
+
+    def process_message(msg):
+        if ob.best_bid == float(msg["data"]["b"]) and ob.best_ask == float(
+            msg["data"]["a"]
+        ):
+            return
+        ob.best_bid = float(msg["data"]["b"])
+        ob.best_ask = float(msg["data"]["a"])
+        trigger_func()
+
+    conn_key = bm.start_symbol_ticker_futures_socket(symbol, process_message)
+    print(f"Subscribe Future: {conn_key}")
+
+    return ob
 
 
 """
@@ -93,31 +163,82 @@ Util
 """
 
 
-def buy_and_short(symbol: str, volume: float, bypass_price_check: bool = False):
-    spot_ob = get_orderbook_ticker(symbol=symbol)
-    future_ob = futures_orderbook_ticker(symbol=symbol)
-
-    if bypass_price_check or future_ob["bidPrice"] > spot_ob["askPrice"]:
-        futures_short_limit(symbol=symbol, price=future_ob["bidPrice"], volume=volume)
-        buy_limit(symbol=symbol, price=spot_ob["askPrice"], volume=volume)
-    else:
-        print("spot price higher than future")
+def buy_and_short(symbol: str, volume: float):
+    buy_market(symbol=symbol, volume=volume)
+    futures_short_market(symbol=symbol, volume=volume)
 
 
-def sell_and_long(symbol: str, volume: float, bypass_price_check: bool = False):
-    spot_ob = get_orderbook_ticker(symbol=symbol)
-    future_ob = futures_orderbook_ticker(symbol=symbol)
+def sell_and_long(symbol: str, volume: float):
+    sell_market(symbol=symbol, volume=volume)
+    futures_long_market(symbol=symbol, volume=volume)
 
-    if bypass_price_check or spot_ob["bidPrice"] > future_ob["askPrice"]:
-        futures_long_limit(symbol=symbol, price=future_ob["bidPrice"], volume=volume)
-        sell_limit(symbol=symbol, price=spot_ob["askPrice"], volume=volume)
-    else:
-        print("future price higher than spot")
+
+def sub_premium_buy(
+    symbol: str,
+    min_premium: float,
+    trigger_func: Callable,
+    args: list = list(),
+    kwargs: dict = dict(),
+):
+    def check_premium():
+        try:
+            premium = future_ob.best_bid / spot_ob.best_ask - 1
+        except TypeError as e:
+            print(e)
+            return
+        print(f"Premium {premium:.8f}")
+        if premium > min_premium:
+            bm.close()
+            trigger_func(*args, **kwargs)
+
+    spot_ob = sub_symbol_ticker_socket(symbol=symbol, trigger_func=check_premium)
+    future_ob = sub_symbol_ticker_futures_socket(
+        symbol=symbol, trigger_func=check_premium
+    )
+    bm.start()
+
+
+def sub_premium_sell(
+    symbol: str,
+    max_premium: float,
+    trigger_func: Callable,
+    args: list = list(),
+    kwargs: dict = dict(),
+):
+    def check_premium():
+        try:
+            premium = future_ob.best_ask / spot_ob.best_bid - 1
+        except TypeError as e:
+            print(e)
+            return
+        print(f"Premium {premium:.8f}")
+        if premium < max_premium:
+            bm.close()
+            trigger_func(*args, **kwargs)
+
+    spot_ob = sub_symbol_ticker_socket(symbol=symbol, trigger_func=check_premium)
+    future_ob = sub_symbol_ticker_futures_socket(
+        symbol=symbol, trigger_func=check_premium
+    )
+    bm.start()
 
 
 if __name__ == "__main__":
-    symbol = "DENTUSDT"
-    volume = 1000
+    symbol = "BTTUSDT"
+    volume = 100
 
-    buy_and_short(symbol=symbol, volume=volume)
-    # sell_and_long(symbol=symbol, volume=volume, bypass_price_check=True)
+    sub_premium_buy(
+        symbol=symbol,
+        min_premium=0.002,
+        trigger_func=buy_and_short,
+        kwargs={"symbol": symbol, "volume": volume},
+    )
+
+    # sub_premium_sell(
+    #     symbol=symbol,
+    #     max_premium=0.001,
+    #     trigger_func=sell_and_long,
+    #     kwargs={"symbol": symbol, "volume": volume},
+    # )
+
+    Timer(interval=60, function=bm.close).start()
